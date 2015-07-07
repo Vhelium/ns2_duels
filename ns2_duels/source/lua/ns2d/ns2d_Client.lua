f526e5379b065d7c0af55431aceec10779aef8e3
Vhelium <vheliumgamestudios@hotmail.com>
2015-07-07 15:22:03 +0200

    working on RoomManager

--- ns2_duels/source/lua/ns2d/ns2d_Client.lua
+++ ns2_duels/source/lua/ns2d/ns2d_Client.lua
@@ -7,6 +7,7 @@ Script.Load("lua/ns2d/ns2d_Armory_Client.lua")
 
 local function OnLoadComplete()
 	Shared.ConsoleCommand("cheats 1")
+	Shared.ConsoleCommand("alltech 1")
 	Shared.Message("OnLoadComplete. Now loading custom GUI...")
 	Script.Load("lua/ns2d/ns2d_GUIScripts.lua")
 end
--- ns2_duels/source/lua/ns2d/ns2d_Shared.lua
+++ ns2_duels/source/lua/ns2d/ns2d_Shared.lua
@@ -1,3 +1,12 @@
+
+Script.Load( "lua/ns2d/ns2d_RoomManager.lua" )
+
+globalRoomManager = RoomManager()
+
+function getRoomManager()
+	return globalRoomManager
+end
+
 Script.Load( "lua/ns2d/Elixer_Utility.lua" )
 Elixer.UseVersion( 1.8 )
 
